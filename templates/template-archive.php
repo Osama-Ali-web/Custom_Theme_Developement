eb740995725c93ca14dd441d7183b3c247435433
ikonic <ikonic@DESKTOP-HAFAI57>
2025-04-25 16:04:35 -0400

    Template file

--- templates/template-archive.php
+++ templates/template-archive.php
@@ -1,32 +1,21 @@
-<?php
-
-get_header();
-?>
-
-
-
-
-
-
-<?php
-
-if ( have_posts() ) : while ( have_posts() ){
-    the_post();
-    get_template_part( 'template-parts/content', get_post_type() );
-}
-
-
-
-endif;
-
-the_content();
-?>
+     <?php
+        /*
+      * Template Name: Custom Product Archive
+      */
+        get_header();
+        ?>
+     <!-- Your archive content here -->
+     <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post();
+        ?>
+             <h2><?php the_title(); ?></h2>
+             <p><?php the_excerpt(); ?></p>
+     <?php
+            endwhile;
+        endif;
+        ?>
 
 
 
-
-
-<?php 
-get_footer();
-?>
-
+     <?php get_footer(); ?>
